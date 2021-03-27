3f54104b2f900ebec5851521a03db25155cb0e6a
HongJunHyeok <48292190+Hong-JunHyeok@users.noreply.github.com>
2021-03-27 22:50:21 +0900

    UPDATE Github Card Styling

--- src/components/Github/GithubCard/GithubCard.tsx
+++ src/components/Github/GithubCard/GithubCard.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import './GithubCard.scss';
 import { GithubUser } from 'modules/github/types';
+import { BsFillPeopleFill } from 'react-icons/bs';
 
 type PropTypes = GithubUser;
 
-const GithubCard = ({ name, email, followers, following, created_at }: PropTypes) => {
+const GithubCard = ({ name, login, bio, followers, following, avatar_url }: PropTypes) => {
   return (
     <div className="GithubCard">
-      <h1>{name}</h1>
-      <h6>{email}</h6>
-      <span className="GithubCard_followers">{followers}</span>
-      <span className="GithubCard_following">{following}</span>
-      <span>{created_at}</span>
+      <img src={avatar_url} alt="내 깃허브 이미지" className="GithubCard_avatar" />
+      <h1 className="GithubCard_name">{name}</h1>
+      <h6 className="GithubCard_login">{login}</h6>
+      <span className="GithubCard_bio">{bio}</span>
+      <span className="GithubCard_followers">
+        <BsFillPeopleFill className="GithubCard_followers_icon" />
+        followers : {followers}
+      </span>
+      <span className="GithubCard_following">following : {following}</span>
     </div>
   );
 };
